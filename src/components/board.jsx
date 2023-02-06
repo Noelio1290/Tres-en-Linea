42b5df1ad7e810d41ee789def0e33e282f934bfb
Noel Zamora <noelio1290@gmail.com>
2023-02-06 15:35:29 -0600

    los componentes ya interactuan entre ellos pero board controla todo

--- src/components/board.jsx
+++ src/components/board.jsx
@@ -2,9 +2,28 @@ import React from "react"
 import Square from "./square"
 
 class Board extends React.Component {
-    renderSquare(i){
-        return <Square value={i} />
+    constructor(props) {
+        super(props);
+        this.state = {
+            squares: Array(9).fill(null),
+        }
+    };
+
+    handleClick(i) {
+        const squares = 
+        this.state.squares.slice();
+        squares[i] = 'X';
+        this.setState({squares:squares})
     }
+
+    renderSquare(i){
+        return (
+            <Square 
+                value={this.state.squares[i]} 
+                onClick={() => this.handleClick(i)}
+            />
+        )
+    };
 
     render(){
         const status = "Next player: X";
